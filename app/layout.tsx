e8c809672769c326caf06aecbf21d2e2e10cfb00
FahadBinAbdullah7 <78896691+FahadBinAbdullah7@users.noreply.github.com>
2025-08-01 19:03:48 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,14 +25,15 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
+      </head>
+      <body className={inter.className}>
+        {children}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
           crossOrigin="anonymous"
-          async
         />
-      </head>
-      <body className={inter.className}>{children}</body>
+      </body>
     </html>
   )
 }
